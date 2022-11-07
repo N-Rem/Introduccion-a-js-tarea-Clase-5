74d4818940817bf280bc53dade3d2d0acbc686ec
N-Rem <nicolasromero.barrios@gmail.com>
2022-11-07 20:38:22 -0300

    agrega funcion a $botonCalcularTiempo
    Suma todo el tiempo de los formularios y los impirme de una forma legible.

--- tarea-tres.js
+++ tarea-tres.js
@@ -31,3 +31,61 @@ function agregarNuevoFormulario(numeroClases) {
 
 
 
+$botonCalcularTiempo.onclick = function () {
+    const numeroDeClases = Number(document.querySelector("#numero-clases").value);
+    const $resultadoFinal = document.querySelector("#tiempo-total");
+    document.querySelector("#contenedor-resultado").classList.add("resultado-final");
+    let horasTotales = 0;
+    let minutosTotales = 0;
+    let segundosTotales = 0;
+    for (let i = 1; i <= numeroDeClases; i++) {
+        horasTotales += Number(document.querySelector(`#horas-clase${i}`).value);
+        minutosTotales += Number(document.querySelector(`#minutos-clase${i}`).value);
+        segundosTotales += Number(document.querySelector(`#segundos-clase${i}`).value);
+
+    }
+    $resultadoFinal.innerText = prepararImprecionResultado(horasTotales, minutosTotales, segundosTotales);
+}
+
+function prepararImprecionResultado(horas, minutos, segundos) {
+    const SEG_MIN_EN_UN_MIN_SEG = 60;
+    let segundosFinales = 0;
+    let minutosFinales = 0;
+    let horasFinales = 0;
+
+
+    if (segundos < SEG_MIN_EN_UN_MIN_SEG) {
+        segundosFinales += segundos
+    }
+    else if (segundos % SEG_MIN_EN_UN_MIN_SEG === 0) {
+        minutosFinales += (segundos / SEG_MIN_EN_UN_MIN_SEG);
+    }
+    else {
+        segundosFinales += (segundos % SEG_MIN_EN_UN_MIN_SEG)
+        minutosFinales += ((segundos - segundosFinales) / SEG_MIN_EN_UN_MIN_SEG);
+    }
+
+
+    if (minutos < SEG_MIN_EN_UN_MIN_SEG) {
+        minutosFinales += minutos;
+    }
+    else if (minutos % SEG_MIN_EN_UN_MIN_SEG === 0) {
+        horasFinales += (minutos / SEG_MIN_EN_UN_MIN_SEG);
+    }
+    else {
+        minutosFinales += (minutos % SEG_MIN_EN_UN_MIN_SEG);
+        horasFinales += ((minutos - (minutos % SEG_MIN_EN_UN_MIN_SEG)) / SEG_MIN_EN_UN_MIN_SEG)
+    }
+
+
+    if (minutosFinales > SEG_MIN_EN_UN_MIN_SEG) {
+        minutosFinales = (minutosFinales % SEG_MIN_EN_UN_MIN_SEG)
+        horasFinales += ((minutosFinales - (minutosFinales % SEG_MIN_EN_UN_MIN_SEG)) / SEG_MIN_EN_UN_MIN_SEG)
+    }
+    horasFinales += horas;
+
+    return `El timepo total de los cursos es ${horasFinales} horas, ${minutosFinales} minutos, ${segundosFinales} segundos`;
+}
+
+
+
